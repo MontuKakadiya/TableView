b2884e88d097eadaf851401e3b1744d9ae8418d1
ChavdaKishan <chavdakish1996@gmail.com>
2017-06-22 11:53:49 +0530

    1

--- Tableview_Demo/ViewController.swift
+++ Tableview_Demo/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview.dataSource = self
+        tableview.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 5
+        }
+        else if section == 1 {
+            return 5
+        }
+        else if section == 2 {
+            return 5
+        }
+        else if section == 3 {
+            return 5
+        }
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell : UITableViewCell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: "Cell Identifier")
+        
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.section) - \(indexPath.row)")
 
-
+    }
+    
+    
 }
 
